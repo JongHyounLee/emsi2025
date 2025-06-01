218d00205cfb615ac4765fcd69e8869ceca9bcbc
Jaewon_Ahn <jaewonahn1234@gmail.com>
2025-06-01 19:50:12 +0900

    Update : 소켓 통신 keep sending and receiving message

--- socket/client.c
+++ socket/client.c
@@ -1,4 +1,3 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,29 +5,46 @@
 #include <arpa/inet.h>
 
 #define PORT 8888
-#define SERVER_ADDR "192.168.XXX.XXX"
+#define SERVER_ADDR "192.168.137.189"  // <- Kit2's server addr
+int count = 0;
 int main() {
     int sock;
     struct sockaddr_in server_addr;
-    char *message = "hello world";
 
+    // message to send
+    const char *messages[] = {"Tlqkf\n"};
+    int num_messages = sizeof(messages) / sizeof(messages[0]);
+
+    // create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("socket failed");
         return 1;
     }
 
+    // set server addr 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    
-    // 여기에 키트2의 IP 주소를 넣어야 함
     inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
 
-    connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    send(sock, message, strlen(message), 0);
+    // connect to server
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect failed");
+        close(sock);
+        return 1;
+    }
 
-    printf("메시지 전송 완료\n");
+    printf("Connected to Server! \n");
+
+    // send message
+    while (1) {
+        for (int i = 0; i < num_messages; i++) {
+            send(sock, messages[i], strlen(messages[i]), 0);
+            printf("Send message: %s\n", messages[i]);
+            sleep(1);  // 1초 대기
+        }
+    }
+
     close(sock);
-
     return 0;
 }
--- socket/server.c
+++ socket/server.c
@@ -1,4 +1,3 @@
-// server.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,33 +6,51 @@
 
 #define PORT 8888
 
-int main() {
+int main()
+{
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     char buffer[1024] = {0};
     socklen_t client_len = sizeof(client_addr);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         perror("socket failed");
         return 1;
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;  // 모든 IP에서 수신
+    server_addr.sin_addr.s_addr = INADDR_ANY; // 모든 IP에서 수신
     server_addr.sin_port = htons(PORT);
 
-    bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     listen(server_fd, 1);
 
-    printf("서버 대기 중...\n");
+    printf("Waiting for message..\n");
 
-    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-    read(client_fd, buffer, sizeof(buffer));
-    printf("수신한 메시지: %s\n", buffer);
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) {
+            perror("accept failed");
+            continue;
+        }
 
-    close(client_fd);
+        printf("connected to client!\n");
+
+        while (1) {
+            memset(buffer, 0, sizeof(buffer));
+            int bytes_read = read(client_fd, buffer, sizeof(buffer));
+            if (bytes_read <= 0) {
+                printf("disconnected to client\n");
+                break;
+            }
+            printf("Received message: %s\n", buffer);
+        }
+
+        close(client_fd);
+    }
+
     close(server_fd);
-
     return 0;
 }
