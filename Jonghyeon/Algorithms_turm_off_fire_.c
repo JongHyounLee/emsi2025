63f622f6c1f6978b07193c0637708cdffd24814b
JongHyounLee <162541695+JongHyounLee@users.noreply.github.com>
2025-06-07 16:49:31 +0900

    fire_off

---
+++ Jonghyeon/Algorithms_turm_off_fire_.c
@@ -0,0 +1,63 @@
+int main()
+{
+    if(water_butten)
+    {
+        if(cursor_pos_x==-1)
+        {
+            swich(cursor_pos_y):
+            case -1:
+            {
+                if(Fire_pos[6]==1)
+                Fire_6 =0;
+            }break;
+            case 0:
+            {
+                if(Fire_pos[3]==1)
+                Fire_3 =0;
+            }break;
+            case 1:
+            {
+                if(Fire_pos[0]==1)
+                Fire_0 =0;
+            }break;
+        }
+        if(cursor_pos_x==0)
+        {
+            swich(cursor_pos_y):
+            case -1:
+            {
+                if(Fire_pos[7]==1)
+                Fire_7 =0;
+            }break;
+            case 0:
+            {
+                if(Fire_pos[4]==1)
+                Fire_4 =0;
+            }break;
+            case 1:
+            {
+                if(Fire_pos[1]==1)
+                Fire_1 =0;
+            }break;
+        }
+        if(cursor_pos_x==1)
+        {
+            swich(cursor_pos_y):
+            case -1:
+            {
+                if(Fire_pos[8]==1)
+                Fire_8 =0;
+            }break;
+            case 0:
+            {
+                if(Fire_pos[5]==1)
+                Fire_5 =0;
+            }break;
+            case 1:
+            {
+                if(Fire_pos[2]==1)
+                Fire_2 =0;
+            }break;
+        }
+    }
+}
